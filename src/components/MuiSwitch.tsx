e334f26abf6092e0abab76795e738c8a5b92fc7a
Satyum <satyumspathania@gmail.com>
2023-03-29 18:44:57 +0530

    Switches

---
+++ src/components/MuiSwitch.tsx
@@ -0,0 +1,15 @@
+import {Box,FormControlLabel,Switch} from '@mui/material'
+import {useState} from'react'
+
+export const MuiSwitch = () => {
+    const [checked, setChecked] = useState(false)
+    console.log({checked})
+    const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => {
+        setChecked(event.target.checked)
+    }
+  return (
+    <Box>
+      <FormControlLabel label='Dark Mode' control={<Switch checked={checked} onChange={handleChange} size='small' color='success'></Switch>} />
+    </Box>
+  )
+}
