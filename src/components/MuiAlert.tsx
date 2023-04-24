0334431683a4b75c43111fabd38c68fc933b3661
Satyum <satyumspathania@gmail.com>
2023-04-24 23:40:49 +0530

    working on alert

---
+++ src/components/MuiAlert.tsx
@@ -0,0 +1,11 @@
+import {Stack,Alert,AlertTitle,Button} from '@mui/material'
+import CheckIcon from '@mui/icons-material/Check'
+
+
+export const MuiAlert = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
